b8919c1f8b92935778f89b8d58b1157a4730c118
lnd4812 <laureldavid64@gmail.com>
2022-03-16 21:13:54 -0400

    added saveTasks function and associated coding

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -54,11 +54,10 @@ var createTaskEl = function(taskDataObj) {
   listItemEl.appendChild(taskActionsEl);
   tasksToDoEl.appendChild(listItemEl);
 
-  console.log(taskDataObj);
-  console.log(taskDataObj.status);
-
   taskDataObj.id = taskIdCounter;
   tasks.push(taskDataObj);
+
+  saveTasks();
 
   // increase task counter for next unique id
   taskIdCounter++;
@@ -119,6 +118,8 @@ var completeEditTask = function(taskName, taskType, taskId) {
     }
   };
 
+  saveTasks();
+
   alert("Task Updated!");
 
   // remove data attribute from form
@@ -167,7 +168,7 @@ var taskStatusChangeHandler = function(event) {
       tasks[i].status = statusValue;
     }
   }
-  console.log(tasks);
+  saveTasks();
 };
 
 var editTask = function(taskId) {
@@ -211,7 +212,15 @@ var deleteTask = function(taskId) {
   }
   // reassing tasks array to be the same as updatedTaskArr
   tasks = updatedTaskArr;
+
+  saveTasks();
 };
+
+var saveTasks = function() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// these are "addEventListener METHODS"
 
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
